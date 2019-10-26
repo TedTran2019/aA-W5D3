efd2cd877dfa625c98cdd067006c6644d44d339d
Ted Tran <TedTran2019@gmail.com>
2019-10-25 23:03:01 -0700

    Completed part 1-5 of RailsLite. Part 6 is bugged with the integration test, will debug soon

--- rails_lite/lib/session.rb
+++ rails_lite/lib/session.rb
@@ -4,16 +4,22 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @session = req.cookies["_rails_lite_app"]
+    @session = (@session.nil? ? {} : JSON.parse(@session))
   end
 
   def [](key)
+    @session[key]
   end
 
   def []=(key, val)
+    @session[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    cookie = { path: '/', value: @session.to_json }
+    res.set_cookie('_rails_lite_app', cookie)
   end
 end
